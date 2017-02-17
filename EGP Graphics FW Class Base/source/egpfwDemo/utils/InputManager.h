72e3ac145557e399dffa61bbfa4950e9f0a63935
Wednesday-David Hartman <wscones@gmail.com>
2017-02-16 22:00:52 -0500

    [ADDED] InputManager.h and basic adjustment of parameters. TODO: add the ability to adjust deltas

---
+++ EGP Graphics FW Class Base/source/egpfwDemo/utils/InputManager.h
@@ -0,0 +1,30 @@
+//
+//  InputManager.h
+//  egpfwDemo
+//
+//  Created by Wednesday-David Hartman on 2/16/17.
+//  Copyright 2017 Dan Buckstein. All rights reserved.
+//
+
+#ifndef __INPUTMANAGER_H
+#define __INPUTMANAGER_H
+
+namespace wh
+{
+	enum ParameterOptions
+	{
+		COLOR,
+		VELOCITY,
+		LIFESPAN,
+		MASS
+	};
+
+	enum ParameterSuboptions
+	{
+		NONE,
+		X, Y, Z, W
+	};
+}
+
+
+#endif // __INPUTMANAGER_H
